9db56aad331d091f2b81610c75279111fd943df3
Jonas Amundsen <jonasba@gmail.com>
2015-04-18 14:06:34 +0200

    Add a color scheme for ruby

--- lib/jiffy/outputters/ruby.rb
+++ lib/jiffy/outputters/ruby.rb
@@ -7,18 +7,18 @@ class Jiffy
       rule :end_array, :payload => "]", :indent => -1, :break_before => true
       rule :value_separator, :payload => ",", :break_after => true
       rule :name_separator, :payload => " => "
-      rule :begin_name, :payload => "\""
-      rule :end_name, :payload => "\""
-      rule :begin_string, :payload => "\""
-      rule :end_string, :payload => "\""
-      rule :null, :payload => "nil"
-      rule :true, :payload => "true"
-      rule :false, :payload => "false"
-      rule :char
-      rule :number
-      rule :exponent, :payload => "e"
-      rule :minus, :payload => "-"
-      rule :plus, :payload => "+"
+      rule :begin_name, :payload => "\"", :color => :light_purple
+      rule :end_name, :payload => "\"", :color => :light_purple
+      rule :begin_string, :payload => "\"", :color => :light_purple
+      rule :end_string, :payload => "\"", :color => :light_purple
+      rule :null, :payload => "nil", :color => :purple
+      rule :true, :payload => "true", :color => :purple
+      rule :false, :payload => "false", :color => :purple
+      rule :char, :color => :purple
+      rule :number, :color => :purple
+      rule :exponent, :payload => "e", :color => :purple
+      rule :minus, :payload => "-", :color => :purple
+      rule :plus, :payload => "+", :color => :purple
       rule :escaped_quotation_mark, :payload => "\""
       rule :escaped_reverse_solidus, :payload => "\\"
       rule :escaped_solidus, :payload => "/"
